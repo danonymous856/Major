6776d61f78af22925b9099b635bbdd930c90be2e
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2021-12-09 21:34:50 +0530

    just pull the files in the new folder

---
+++ client/board.py
@@ -0,0 +1,20 @@
+# this is the board programm
+class Board(object):
+    ROWS=COLS=720
+    def __init__(self):
+        self.data = self.create_empty_board()
+
+    def update(self,x,y,color):
+        self.data[y][x]=color
+
+    def clear(self):
+        self.data=self.create_empty_board()
+
+    def create_empty_board(self):
+        return [[(255,255,255) for _ in range(self.COLS)] for _ in range(self.ROWS)]
+
+    def fill(self,x,y):
+        pass
+
+    def get_board(self):
+        return self.data
