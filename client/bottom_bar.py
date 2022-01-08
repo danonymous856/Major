ea5f783b83de24cdfb74fb0e07a064ec8446ebcf
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2022-01-08 12:07:57 +0530

    client's bottom bar filled up with button_events,draw functions.

--- client/bottom_bar.py
+++ client/bottom_bar.py
@@ -22,7 +22,35 @@ class BottomBar:
         self.BORDER_THICKNESS = 5
         self.clear_button = TextButton(self.x + self.WIDTH - 150,self.y + 25,100,50,(128,128,128),"clear")
         self.eraser_button = TextButton(self.x + self.WIDTH - 300,self.y + 25,100,50,(128,128,128),"Eraser")
-        self.color_button = [Button(self.x + 20,self.y +5, 30, 30 self.COLORS[0]),
+        self.color_button = [Button(self.x + 20,self.y +5, 30,30,self.COLORS[0]),
         Button(self.x +50,self.y + 5,30,30,self.COLORS[1]),
-        Button(self.x + 80,self.)]
-        
+        Button(self.x + 80,self.y + 5,30,30,self.COLORS[2]),
+        Button(self.x + 20,self.y + 35,30,30,self.COLORS[3]),
+        Button(self.x + 50,self.y + 35,30,30,self.COLORS[4]),
+        Button(self.x + 80,self.y + 35,30,30,self.COLORS[5]),
+        Button(self.x + 20,self.y + 65,30,30,self.COLORS[6]),
+        Button(self.x + 50,self.y + 65,30,30,self.COLORS[7]),
+        Button(self.x + 80,self.y + 65,30,30,self.COLORS[8])]
+
+    def draw(self,win):
+        pygame.draw.rect(win,(0,0,0),(self.x,self.y,self.WIDTH,self.HEIGHT),self.BORDER_THICKNESS)
+        self.clear_button.draw(win)
+        self.eraser_button.draw(win)
+
+    def button_events(self):"""
+        handle all button press events here
+        :return: None
+        """
+
+        mouse = pygame.mouse.get_pos()
+
+        if self.clear_button.click(*mouse):
+            self.game.board.clear()
+            self.game.connection.send({10:[]})
+
+        if self.eraser_button.click(*mouse):
+            self.game.draw_color = (255,255,255)
+
+        for btn in self.color_button:
+            if btn.click(*mouse):
+                self.game.draw_color = btn.color
