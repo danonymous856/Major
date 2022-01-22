9c4f2444d38dda1b01447320ef3c1149b889cb3f
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2022-01-22 23:25:36 +0530

    Created "translate" and "create" function  .Also created "draw" function .

--- Pictionaries/client/board.py
+++ Pictionaries/client/board.py
@@ -35,4 +35,11 @@ def create_board(self):
 def translate_board(self):
     for y, _ in enumerate(self.compressed):
         for x, col in enumerate(self.compressed_board[y]):
+            self.board[y][x] = self.COLORS[col]
+
+def draw(self,win):
+    pygame.draw.rect(win,(0,0,0),(self.BORDER_THICKNESS/2,self.y-self.BORDER_THICKNESS/2,self.WIDTH + self.BORDER_THICKNESS,self.HEIGHT +self.BORDER_THICKNESS),self.BORDER_THICKNESS)
+    for y,_ in enumerate(self.board):
+        for x, col in enumerate(self.board[y]):
+        pygame.draw.rect(win,col,(self.x +x*8,self,y +y*8,8,8),0)
 
