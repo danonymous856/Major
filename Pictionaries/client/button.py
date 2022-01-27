ec40c4adddfe7c778d2ccaadd67a6de67736cc00
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2022-01-28 00:01:51 +0530

    Just created the button .py doc in the client directory .

---
+++ Pictionaries/client/button.py
@@ -0,0 +1,9 @@
+"""
+Stores interface for button and the two concreate button classes to be used in the UI .
+"""
+
+import pygame
+
+class Button:
+    def __init__(self,x,y,width,height,color,border_color = (0,0,0)):
+        self.x = x
