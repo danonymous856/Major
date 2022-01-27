660f4b62f5e6b26b2c0e766db5d9f33d65ab7d0c
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2022-01-27 22:39:49 +0530

    updated the very board

--- Pictionaries/client/board.py
+++ Pictionaries/client/board.py
@@ -41,5 +41,6 @@ def draw(self,win):
     pygame.draw.rect(win,(0,0,0),(self.BORDER_THICKNESS/2,self.y-self.BORDER_THICKNESS/2,self.WIDTH + self.BORDER_THICKNESS,self.HEIGHT +self.BORDER_THICKNESS),self.BORDER_THICKNESS)
     for y,_ in enumerate(self.board):
         for x, col in enumerate(self.board[y]):
-        pygame.draw.rect(win,col,(self.x +x*8,self,y +y*8,8,8),0)
+            pygame.draw.rect(win,col,(self.x +x*8,self,y +y*8,8,8),0)
+        
 
